c5099db50b769c9d18169f324c8779a5d06576d9
Nada Alsogour <99046134+Nada-bit73@users.noreply.github.com>
2022-07-06 20:56:40 +0300

    Update settings.py

--- TheWall/settings.py
+++ TheWall/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f@)+9mws$^w72^-8on$uo-5y%%o*88$ayc#0+j#b6cw7hb8f(e'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
